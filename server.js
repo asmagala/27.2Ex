b1880d23dba10d155574fa3b4d3799419783a0e3
asmagala <asmagala@op.pl>
2020-05-28 14:37:09 +0200

    Init.

--- server.js
+++ server.js
@@ -1,35 +1,51 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
-
-app.use((req, res, next) => {
-  res.show = (name) =>{
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
+/*
 app.use('/user', (req, res) => {
-  res.show('forbidden.html');
+  res.render('forbidden', { layout: false });
 });
+*/
 
 app.get('/', (req, res) => {
-  res.show('index.html');
+res.render('index', { layout: false });
 });
 
-app.get('/home', (req, res) => {
-  res.show('index.html');
+/*
+app.get('/home/', (req, res) => {
+  res.render('index', { layout: false });
 });
+*/
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about', { layout: false });
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { layout: false, name: req.params.name });
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact', { layout: false });
+});
+
+app.get('/info', (req, res) => {
+  res.render('info', { layout: false });
+});
+
+app.get('/history', (req, res) => {
+  res.render('history', { layout: false });
 });
 
 app.use((req, res) => {
-  res.status(404).show('404.html');
+  res.status(404).render('404', { layout: false });
 });
 
 app.listen(8000, () => {
